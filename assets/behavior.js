2d502ba3aeed5d5b0f72a3118171dcece2aec7b6
Alexander Cherniuk <ts33kr@gmail.com>
2013-08-31 14:17:54 +0300

    Generated documentation for c6e09eb57e0ecdeadb9e81a79a0d294a36ab10c6

--- assets/behavior.js
+++ assets/behavior.js
@@ -143,6 +143,22 @@ f.event={add:function(a,c,d,e,g){var h,i,j,k,l,m,n,o,p,q,r,s;if(!(a.nodeType===3
               "singleLineComment": ["#"],
               "name": "CoffeeScript"
             },
+            "sourcePath": "/Users/ts33kr/Projects/granite/library/membrane/preflight.coffee",
+            "projectPath": "library/membrane/preflight.coffee",
+            "targetPath": "membrane/preflight",
+            "title": "preflight"
+          },
+          "depth": 2,
+          "outline": []
+        }, {
+          "type": "file",
+          "data": {
+            "language": {
+              "nameMatchers": [".coffee", "Cakefile"],
+              "pygmentsLexer": "coffee-script",
+              "singleLineComment": ["#"],
+              "name": "CoffeeScript"
+            },
             "sourcePath": "/Users/ts33kr/Projects/granite/library/membrane/primitive.coffee",
             "projectPath": "library/membrane/primitive.coffee",
             "targetPath": "membrane/primitive",
