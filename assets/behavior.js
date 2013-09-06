b39f6a7e356bd7f4e986f3f501b5113f423045af
Alexander Cherniuk <ts33kr@gmail.com>
2013-09-06 12:21:54 +0300

    Generated documentation for deca9d98a21a1e99bee00147113670753ea76763

--- assets/behavior.js
+++ assets/behavior.js
@@ -32,8 +32,8 @@ f.event={add:function(a,c,d,e,g){var h,i,j,k,l,m,n,o,p,q,r,s;if(!(a.nodeType===3
           "type": "heading",
           "data": {
             "level": 2,
-            "title": "Concept",
-            "slug": "concept"
+            "title": "Concepts",
+            "slug": "concepts"
           },
           "depth": 2
         }
