3f194d730286b46f1a85906774a4628732b2275b
Alex Cherniuk <ts33kr@gmail.com>
2014-06-01 22:14:10 +0300

    Generated documentation for d43e8cbd09008350b80b878c114ba3490d088450

--- assets/behavior.js
+++ assets/behavior.js
@@ -202,6 +202,27 @@ f.event={add:function(a,c,d,e,g){var h,i,j,k,l,m,n,o,p,q,r,s;if(!(a.nodeType===3
               "foldPrefix": "^",
               "name": "CoffeeScript"
             },
+            "sourcePath": "/Users/ts33kr/Projects/granite/library/exposure/pinpoint.coffee",
+            "projectPath": "library/exposure/pinpoint.coffee",
+            "targetPath": "exposure/pinpoint",
+            "pageTitle": "exposure/pinpoint",
+            "title": "pinpoint"
+          },
+          "depth": 2,
+          "outline": []
+        }, {
+          "type": "file",
+          "data": {
+            "language": {
+              "nameMatchers": [".coffee", "Cakefile"],
+              "pygmentsLexer": "coffee-script",
+              "multiLineComment": ["###*", " *", " ###", "###", "#", "###", "###*", "#", "###"],
+              "strictMultiLineEnd": false,
+              "singleLineComment": ["#"],
+              "ignorePrefix": "}",
+              "foldPrefix": "^",
+              "name": "CoffeeScript"
+            },
             "sourcePath": "/Users/ts33kr/Projects/granite/library/exposure/policies.coffee",
             "projectPath": "library/exposure/policies.coffee",
             "targetPath": "exposure/policies",
