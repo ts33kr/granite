8ce6a5d6f390a5b7b2eb3305216009f29913f57b
Alex Cherniuk <ts33kr@gmail.com>
2014-06-12 22:30:36 +0300

    Generated documentation for 501b4014210c01d8a037a872f09c7e47813ecd5e

--- assets/behavior.js
+++ assets/behavior.js
@@ -443,6 +443,27 @@ f.event={add:function(a,c,d,e,g){var h,i,j,k,l,m,n,o,p,q,r,s;if(!(a.nodeType===3
               "foldPrefix": "^",
               "name": "CoffeeScript"
             },
+            "sourcePath": "/Users/ts33kr/Projects/granite/library/membrane/embed.coffee",
+            "projectPath": "library/membrane/embed.coffee",
+            "targetPath": "membrane/embed",
+            "pageTitle": "membrane/embed",
+            "title": "embed"
+          },
+          "depth": 2,
+          "outline": []
+        }, {
+          "type": "file",
+          "data": {
+            "language": {
+              "nameMatchers": [".coffee", "Cakefile"],
+              "pygmentsLexer": "coffee-script",
+              "multiLineComment": ["###*", " *", " ###", "###", "#", "###", "###*", "#", "###"],
+              "strictMultiLineEnd": false,
+              "singleLineComment": ["#"],
+              "ignorePrefix": "}",
+              "foldPrefix": "^",
+              "name": "CoffeeScript"
+            },
             "sourcePath": "/Users/ts33kr/Projects/granite/library/membrane/events.coffee",
             "projectPath": "library/membrane/events.coffee",
             "targetPath": "membrane/events",
